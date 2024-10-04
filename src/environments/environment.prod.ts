a06bd8acb9b69a59d992c9bbcd76af2104d3f8ae
RhaxCity <juanjogomezarenas1@gmail.com>
2024-10-03 21:12:37 -0500

    configuracion de Repositorio Inicial

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    baseApiUrl: 'http://10.43.100.206:8090', // Cambia esto a la URL base en producción
+};
+  
---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseApiUrl: 'http://10.43.101.180:8090', // Cambia esto a la URL base en el entorno de testing
+  };
+  
