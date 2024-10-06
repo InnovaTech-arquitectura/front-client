27f5f622b8c116aaa868951bcdce735c3dfd718e
RhaxCity <juanjogomezarenas1@gmail.com>
2024-10-05 21:06:51 -0500

    Update registro.component.spec.ts

--- src/app/registro/registro.component.spec.ts
+++ src/app/registro/registro.component.spec.ts
@@ -1,15 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { RegistroComponent } from './registro.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RegistroComponent } from './registro.component'; // Ajusta la ruta si es necesario
+import { AuthService } from '../service/auth.service'; // Asegúrate de ajustar la ruta
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
 
 describe('RegistroComponent', () => {
   let component: RegistroComponent;
   let fixture: ComponentFixture<RegistroComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientModule, FormsModule], // Agrega esto
+      declarations: [RegistroComponent],
+      providers: [AuthService] // Asegúrate de que tu servicio esté proporcionado
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [RegistroComponent]
-    });
     fixture = TestBed.createComponent(RegistroComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
