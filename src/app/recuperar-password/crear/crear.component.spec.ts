1d168c5cb9cf19722736cb14d3a0b199b2e20d50
Sebastian Isaza <dupersebas@gmail.com>
2024-10-08 19:12:47 -0500

    Crear 2.0

--- src/app/recuperar-password/crear/crear.component.spec.ts
+++ src/app/recuperar-password/crear/crear.component.spec.ts
@@ -1,15 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { CrearComponent } from './crear.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { CrearComponent } from './crear.component'; // Asegúrate de tener la ruta correcta
+import { AuthService } from '../../service/auth.service'; // Asegúrate de tener la ruta correcta
 
 describe('CrearComponent', () => {
   let component: CrearComponent;
   let fixture: ComponentFixture<CrearComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterModule.forRoot([]),
+        FormsModule // Asegúrate de incluir esto
+      ],
+      declarations: [CrearComponent],
+      providers: [AuthService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA] // Esto ignora elementos personalizados que no están en las pruebas
+    }).compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [CrearComponent]
-    });
     fixture = TestBed.createComponent(CrearComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -18,4 +32,6 @@ describe('CrearComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Agrega más pruebas según sea necesario
 });
