5321d9f9f67e5d33d3f58d1c0ad4fd8e9655577f
Sebootas <dupersebas@gmail.com>
2024-10-19 22:11:43 -0500

    Pedidos

---
+++ src/app/service/pedido.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pedido } from '../pedido.model'; // Asegúrate de definir el modelo
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PedidoService {
+  private apiUrl = 'http://localhost:8080/api/pedidos'; // Cambia esto según tu configuración
+
+  constructor(private http: HttpClient) {}
+
+  getAllPedidos(): Observable<Pedido[]> {
+    return this.http.get<Pedido[]>(this.apiUrl);
+  }
+
+  getPedidoById(id: number): Observable<Pedido> {
+    return this.http.get<Pedido>(`${this.apiUrl}/${id}`);
+  }
+}
