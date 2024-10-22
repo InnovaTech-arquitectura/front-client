562ef426ef0ccc9e41c78de1d083c99c129e0826
Sebootas <dupersebas@gmail.com>
2024-10-21 19:27:36 -0500

    Specs

--- src/app/service/reserva.service.ts
+++ src/app/service/reserva.service.ts
@@ -2,12 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Reserva } from '../reserva.model'; // Asegúrate de importar el modelo correctamente
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservaService {
-  private apiUrl = 'http://localhost:8080/api/reservas'; // Asegúrate de que la URL coincida con el backend
+  private apiUrl = environment.funcionalidadesUrl + ':8080/api/reservas';
+
+  //private apiUrl = 'http://localhost:8080/api/reservas'; // Asegúrate de que la URL coincida con el backend
 
   constructor(private http: HttpClient) {}
 
