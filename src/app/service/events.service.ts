4f58edc4d4f7e2e8eead71b5b04497628ac981b8
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2024-10-22 23:13:29 -0500

    bazares corregido

--- src/app/service/events.service.ts
+++ src/app/service/events.service.ts
@@ -15,22 +15,18 @@ export class EventsService {
     return this.http.get<any>(`${this.apiUrl}?page=${page}&size=${size}`);
   }
 
-  // Obtener detalles de un bazar
   getBazarDetails(id: number): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 
-  // Agregar un nuevo bazar
   addBazar(event: any): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}/add`, event);
   }
 
-  // Actualizar un bazar
   updateBazar(event: any): Observable<any> {
     return this.http.put<any>(`${this.apiUrl}/update`, event);
   }
 
-  // Eliminar un bazar
   deleteEvent(id: number): Observable<void> {
     return this.http.delete<void>(`${this.apiUrl}/delete/${id}`);
   }
