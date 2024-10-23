84cf12639a51f0ab52f61e174811566c0568d510
Juan José Gómez Arenas <104366230+RhaxCity@users.noreply.github.com>
2024-10-23 11:11:08 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -31,8 +31,17 @@ else
     export SERVER_NAME=10.43.101.180
 fi
 
+# Detener y eliminar contenedores existentes
+echo "Deteniendo y eliminando contenedores existentes..."
+docker-compose down
+
+# Limpiar imágenes huérfanas y no utilizadas
+echo "Limpiando imágenes de Docker no utilizadas..."
+docker image prune -f
+
 # Ejecutar docker-compose con el argumento de entorno
-docker compose up --build -d
+echo "Levantando el contenedor con docker-compose..."
+docker-compose up --build -d
 
 echo "Reiniciando Nginx en el contenedor..."
 docker exec front-client-container nginx -s reload
