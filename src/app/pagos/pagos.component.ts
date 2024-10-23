0c711e1cbdb91557e6a2d7b5041f4e3f4ce5a670
Shermann103 <hhermann103@gmail.com>
2024-10-23 09:25:02 -0500

    Pop-Up confirmPayment

--- src/app/pagos/pagos.component.ts
+++ src/app/pagos/pagos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pagos',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagos.component.css']
 })
 export class PagosComponent {
+
+  confirmPayment(): void {
+    Swal.fire({
+      icon: 'success',
+      title: 'Gracias por su compra',
+      text: 'Su pedido ha sido confirmado',
+    });
+  }
 
 }
