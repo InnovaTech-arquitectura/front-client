199c7aea4d98a1985e489cd6e112cacd068bd0c3
RhaxCity <juanjogomezarenas1@gmail.com>
2024-10-22 22:43:21 -0500

    new Environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
-    usuarioSoporteUrl: 'http://10.43.101.125', // URL específica para usuarioSoporte
-    funcionalidadesUrl: 'http://10.43.100.211', // URL específica para funcionalidades
+    baseApiUrl: 'http://10.43.100.206:8010',
 };
   
