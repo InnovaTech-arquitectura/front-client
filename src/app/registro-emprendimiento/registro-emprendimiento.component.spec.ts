a5443a4e3eaefa073b7cbb9703d46305a0e53737
RhaxCity <juanjogomezarenas1@gmail.com>
2024-11-06 10:27:18 -0500

    fix tests

--- src/app/registro-emprendimiento/registro-emprendimiento.component.spec.ts
+++ src/app/registro-emprendimiento/registro-emprendimiento.component.spec.ts
@@ -1,17 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegistroEmprendimientoComponent } from './registro-emprendimiento.component';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing'; // For HTTP requests in tests
+import { AuthService } from '../service/auth.service';
+import { FormsModule } from '@angular/forms';  // Import FormsModule to use ngModel
+import { RouterTestingModule } from '@angular/router/testing'; // Import RouterTestingModule to handle routerLink
 
 describe('RegistroEmprendimientoComponent', () => {
   let component: RegistroEmprendimientoComponent;
   let fixture: ComponentFixture<RegistroEmprendimientoComponent>;
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [RegistroEmprendimientoComponent],
+      imports: [
+        HttpClientTestingModule, 
+        FormsModule, 
+        RouterTestingModule // Add RouterTestingModule here
+      ],
+      providers: [AuthService], // Ensure AuthService is provided
+    })
+    .compileComponents();
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [RegistroEmprendimientoComponent]
-    });
     fixture = TestBed.createComponent(RegistroEmprendimientoComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService); // Inject AuthService
     fixture.detectChanges();
   });
 
