af0e94ec4ccf3a831842f2dd8985a5453085207b
Sophia Aristizabal <saristizabal1017@gmail.com>
2024-11-16 01:30:08 -0500

    necessar changes in entrepreneurship account. Finally...

--- src/app/entrepreneurship.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Entrepreneurship {
-    id: number; 
-    name: string; 
-    logo: string; 
-    description: string; 
-  }
-  
---
+++ src/app/model/Entrepreneurship.ts
@@ -0,0 +1,8 @@
+export interface Entrepreneurship {
+    NameTitular: string; 
+    Id_card: number; 
+    email: string; 
+    NameEntrepreneurship: string; 
+    Logo: string; 
+    Description: string;
+}
