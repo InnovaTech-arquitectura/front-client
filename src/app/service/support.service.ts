d2a959001721f96e09b33c8935ccecde204de502
RhaxCity <juanjogomezarenas1@gmail.com>
2024-11-16 16:22:52 -0500

    fix support auth token

--- src/app/service/support.service.ts
+++ src/app/service/support.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Question } from '../model/question';
@@ -12,18 +12,26 @@ export class SupportService {
 
   constructor(private http: HttpClient) { }
 
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token'); // Obtén el token del almacenamiento local
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
+
   // Método para obtener preguntas con paginación
   getAllQuestions(page: number, size: number): Observable<Question[]> {
-    return this.http.get<Question[]>(`${this.apiUrl}/questions?page=${page}&size=${size}`);
+    const headers = this.getHeaders();
+    return this.http.get<Question[]>(`${this.apiUrl}/questions?page=${page}&size=${size}`, { headers });
   }
 
   // Método para obtener el conteo total de preguntas
   getQuestionsCount(): Observable<number> {
-    return this.http.get<number>(`${this.apiUrl}/questions/count`);
+    const headers = this.getHeaders();
+    return this.http.get<number>(`${this.apiUrl}/questions/count`, { headers });
   }
 
   // Método para crear una nueva pregunta
   createQuestion(question: Question): Observable<Question> {
-    return this.http.post<Question>(`${this.apiUrl}/questions`, question);
+    const headers = this.getHeaders();
+    return this.http.post<Question>(`${this.apiUrl}/questions`, question, { headers });
   }
 }
