84c4ae76d4eb8befcf1de4a57ec45e04ec6569c4
Sebootas <dupersebas@gmail.com>
2024-11-17 00:41:24 -0500

    Planes + Cupones full Functionality

--- src/app/plan.model.ts
+++ src/app/plan.model.ts
@@ -1,7 +1,8 @@
 export interface Plan {
-    id: number;
-    name: string;
-    price: number;
-    // Otros campos que puedas tener
-  }
-  
+  id: number;
+  name: string;
+  price: number;
+  planId: number;
+  description?: string; // Optional field for the description
+  planFunctionalities?: any[]; // Optional array for functionalities associated with the plan
+}
